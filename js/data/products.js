78073570f4a035d71ee0acfaa6d79861573711e1
Yana <yanaegorova69.3@gmail.com>
2021-08-30 02:49:58 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,148 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Pastry Crimp-Cut-Seal',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Crimp and seal pies, turnovers, ravioli and tarts by rolling around edges of dough.',
+            },
+            {
+                text: 'You\'ll have great looking, securely sealed pastries and pasta in no time.',
+            },
+            {
+                text: 'Slotted metal wheel with a solid wood handle.',
+            },
+            {
+                text: 'Measures 7 inch/18cm long and crimper 2.25"/5.5cm.',
+            },
+            {
+                text: 'Hand washing recommended.',
+            },
+        ],
+        images: ['./img/img1.png', './img/img1-1.png', './img/img1-2.jpg', './img/img1-3.jpeg', './img/img1-4.jpg'],
+    },
+    {
+        id: 'product-2',
+        name: 'Caulking Gun',
+        price: 29.99,
+        discount: 24.65,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'Steel half-barrel (also called cradle) frame caulking gun for 1/10-gal. cartridges.',
+            },
+            {
+                text: 'Thrust ratio of 10:1 for use with low viscosity materials.',
+            },
+            {
+                text: 'Smooth pressure rod requires less force and is quieter than a ratchet-style pressure rod.',
+            },
+            {
+                text: 'Rod retracts after each pull of the trigger to help prevent dripping.',
+            },
+            {
+                text: 'Handle and trigger are padded for comfort.',
+            },
+            {
+                text: 'Professional-grade 1/10-gallon drip-free caulk gun.',
+            },
+            {
+                text: 'Thermoplastic comfort grip.',
+            },
+        ],
+        images: ['./img/img2.png', './img/img2-1.png', './img/img2-2.png' , './img/img2-3.png', './img/img2-4.png', './img/img2-5.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Vacuum Base',
+        price: 64.90,
+        discount: 69.99,
+        image: './img/img3.png',
+        description: [
+            {
+                text: 'Single knob controls head movement through 3-planes: 90 Degree Tilt, 360 Turn & 360 Rotation.',
+            },
+            {
+                text: 'Jaws are 2.5-Inch (63.5mm) wide, 0.5 (12.6mm) tall and open to 2.25 (57.1mm).',
+            },
+            {
+                text: 'Base accepts all 300 series Heads and any 5/8-Inch (15.9mm) diameter shaft.',
+            },
+            {
+                text: 'Pad footprint measures 5.125 x 4.25 (130.2mm x 108mm). Height: 6.125 (155.6mm), Weight: 2.8 lbs. (1.3kg).',
+            },
+        ],
+        images: ['./img/img3.png', './img/img3-1.png', './img/img3-2.png', './img/img3-3.png', './img/img3-4.png'],
+    },
+    {
+        id: 'product-4',
+        name: '',
+        price: 98.90,
+        discount: 55.97,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'One pillow that takes the place of multiple pillows.',
+            },
+            {
+                text: 'Can help provide support for neutral positioning.',
+            },
+            {
+                text: 'Can help provide support to lower back, hips, and tummy.',
+            },
+            {
+                text: 'Use for upper or lower body elevation.',
+            },
+            {
+                text: 'Zippered removable cover is machine washable and dryer safe.',
+            },
+        ],
+        images: ['./img/img4.png', './img/img4-1.png', './img/img4-2.png', './img/img4-3.png'],
+    },
+    {
+        id: 'product-5',
+        name: '',
+        price: 119.98,
+        discount: 79.93,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'Hair Type: Normal.',
+            },
+            {
+                text: 'Color: Yellow Oval.',
+            },
+            {
+                text: 'Special Feature: 2.44" oval shape with gently curved edges provides tons of volume and a smooth finish.',
+            },
+            {
+                text: 'Material: Nylon.',
+            },
+        ],
+        images: ['./img/img5.png', './img/img5-1.png', './img/img5-2.png', './img/img5-3.png', './img/img5-4.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Pasta Roller & Cutter',
+        price: 148.00,
+        discount: 95.50,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Convenient front pocket.',
+            },
+            {
+                text: 'Rolls 6-inch sheets of pasta with 8 thickness settings.',
+            },
+            {
+                text: 'Spaghetti Cutter cuts pasta sheets into classic Spaghetti noodles.',
+            },
+            {
+                text: 'Fettuccine Cutter cuts pasta sheets into hearty Fettuccine noodles.',
+            },
+        ],
+        images: ['./img/img6.png', './img/img6-1.png'],
+    },
+]
