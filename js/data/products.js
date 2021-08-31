92efc9d5a0d72058ed904827576e3a78d7024c1e
Yana <yanaegorova69.3@gmail.com>
2021-08-31 17:13:44 +0300

    +

--- js/data/products.js
+++ js/data/products.js
@@ -2,7 +2,7 @@ export const products = [
     {
         id: 'product-1',
         name: 'Pastry Crimp-Cut-Seal',
-        price: 6.96,
+        price: 19.99,
         image: './img/img1.png',
         description: [
             {
@@ -26,8 +26,8 @@ export const products = [
     {
         id: 'product-2',
         name: 'Caulking Gun',
-        price: 29.99,
-        discount: 24.65,
+        price: 24.99,
+        discount: 14.99,
         image: './img/img2.png',
         description: [
             {
@@ -56,9 +56,109 @@ export const products = [
     },
     {
         id: 'product-3',
-        name: 'Vacuum Base',
-        price: 64.90,
+        name: 'Paper Trimmer',
+        price: 35.32,
+        discount: 24.65,
+        image: './img/img7.png',
+        description: [
+            {
+                text: 'Everyday portable paper trimmer with 15-inch guillotine-style, self-sharpening steel blade; ideal for home or office.',
+            },
+            {
+                text: 'Cleanly cuts up to 10 sheets of 20-pound paper at a time; sharp enough to accurately trim cardstock, construction paper, photos, and more.',
+            },
+            {
+                text: 'Guardrail keeps fingers a safe distance from the blade; when not in use, trimmer arm can be secured in place, fully hiding the blade.',
+            },
+            {
+                text: 'Durable plastic base with white 1/2-inch gridlines (0.5 by 0.5 inches) and a printed ruler (in inches and cm) that allow for reliable alignment and precision cuts.',
+            },
+            {
+                text: 'Measures 21.7 by 14 by 2.6 inches; weighs 3.9 pounds.',
+            },
+        ],
+        images: ['./img/img7.png', './img/img7-1.png', './img/img7-2.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Vacuum Seal System',
+        price: 39.99,
+        discount: 39.97,
+        image: './img/img8.png',
+        description: [
+            {
+                text: 'Vacuum sealer appliance with selectable modes for wet or dry food.',
+            },
+            {
+                text: 'Effectively vacuum seal pre-made meals, produce, meat, dry goods, and more for preservation and freshness.',
+            },
+            {
+                text: 'Safely preserves frozen meat up to 3 years and refrigerated items for weeks.',
+            },
+            {
+                text: 'Includes air suction hose and 16 foot roller bag; LED process indicator lights.',
+            },
+            {
+                text: 'Built-in cutter creates custom bag sizes for easy storage and waste reduction.',
+            },
+        ],
+        images: ['./img/img8.png', './img/img8-1.png', './img/img8-2.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Enameled Cast Iron Oven',
+        price: 43.98,
+        discount: 55.97,
+        image: './img/img9.jpeg',
+        description: [
+            {
+                text: 'Multifunctional laundry sorter and ironing board combination for space-saving convenience.',
+            },
+            {
+                text: 'Flat top surface made of 0.5 inch MDF board with a heat-resistant canvas cover for ironing garments and more; moves 90 degrees for easy access to laundry bags.',
+            },
+            {
+                text: '3 large-capacity, heavy-duty cloth laundry bags that can be removed from frame.',
+            },
+            {
+                text: '2 wheels with locking mechanism for easy mobility between rooms.',
+            },
+            {
+                text: 'Durable powder-coated steel frame and handles; holds up to 132 pounds.',
+            },
+        ],
+        images: ['./img/img9.jpeg', './img/img9-1.jpeg', './img/img9-2.jpeg' , './img/img9-3.jpeg'],
+    },
+    {
+        id: 'product-6',
+        name: 'Fan With Remote',
+        price: 49.95,
         discount: 69.99,
+        image: './img/img10.png',
+        description: [
+            {
+                text: '2-Blade oscillating pedestal fan with automatic oscillation and adjustable height and vertical angle.',
+            },
+            {
+                text: 'Engineered for whisper-quiet operation; High quality silent motor works at low noise level.',
+            },
+            {
+                text: '3 power settings, and 3 breeze modes (nature, sleep, normal) with wide oscillation for cooling medium to large rooms.',
+            },
+            {
+                text: 'Easy digital operation, auto on/off timer, and remote control for changing modes from across the room.',
+            },
+            {
+                text: '120 volt / 55 watt power consumption.',
+            },
+        ],
+        images: ['./img/img10.png', './img/img10-1.png', './img/img10-2.png'],
+    },
+    {
+        id: 'product-7',
+        name: 'Vacuum Base',
+        price: 69.99,
+        discount: 69.94,
         image: './img/img3.png',
         description: [
             {
@@ -77,10 +177,66 @@ export const products = [
         images: ['./img/img3.png', './img/img3-1.png', './img/img3-2.png', './img/img3-3.png', './img/img3-4.png'],
     },
     {
-        id: 'product-4',
+        id: 'product-8',
+        name: 'Binoculars For Adults',
+        price: 79.99,
+        discount: 79.93,
+        image: './img/img11.png',
+        description: [
+            {
+                title: 'Multi-coated optics and bak-4 prisms',
+                text: 'Multi-coated optics help obtain high resolution and high contrast views, while the prisms made of BaK-4 glass will give you enhanced color fidelity.',
+            },
+            {
+                title: 'Waterproof and fogproof',
+                text: 'Designed to withstand all weather conditions, our compact binoculars have been filled and sealed with dry nitrogen gas for rigorous outdoor use without internal fogging of the lenses.',
+            },
+            {
+                title: 'Protective rubber covering',
+                text: 'Our binocular is both durable and well-armored; its rugged exterior ensures protection from all outdoor elements while providing a secure, anti-slip gripping surface.',
+            },
+            {
+                title: 'Twist-up eyecups for quick adjustment',
+                text: 'Large focus knobs make for easy and precise focusing, and twist-up eyecups allow for quick adjustment of eye relief to obtain a full field of view. Eyecups can be easily positioned for use with or without eyeglasses.',
+            },
+        ],
+        images: ['./img/img11.png', './img/img11-1.png', './img/img11-2.png', './img/img11-3.png'],
+    },
+    {
+        id: 'product-9',
+        name: 'Folding Wagon For Kids And Cargo',
+        price: 84.41,
+        discount: 95.55,
+        image: './img/img12.png',
+        description: [
+            {
+                title: '3 wagons in 1',
+                text: 'This collapsible wagon has 3 riding features: hauling, 2 rider seating and bench seating. Easily transform from hauling to bench seating with the zippers on the side of the wagon.',
+            },
+            {
+                title: 'Folds for storage',
+                text: 'Easy one hand fold allows convenient storage. This wagon is portable and can be stored or taken anywhere! Folded wagon dimensions: 7” L x 18.5” W x 32.68” H',
+            },
+            {
+                title: 'Safe for kids',
+                text: 'Wagons pass the ASTM-f963-13 toy safety standard. This wagon includes seat belts and a padded seat.',
+            },
+            {
+                title: 'Storage capacity',
+                text: 'The wagon can haul up to 150 lbs. of kids or cargo! It also includes a rear storage loops and two front cup holders.',
+            },
+            {
+                title: 'Specifications',
+                text: 'Assembled product dimensions: 39.4” L x 21.2” W x 32.7” H. Recommended for kids 1 ½+ years. Maximum weight capacity 150 lbs',
+            },
+        ],
+        images: ['./img/img12.png', './img/img12-1.png', './img/img12-2.png', './img/img12-3.png'],
+    },
+    {
+        id: 'product-10',
         name: '',
-        price: 98.90,
-        discount: 55.97,
+        price: 89.99,
+        discount: 109.91,
         image: './img/img4.png',
         description: [
             {
@@ -102,10 +258,10 @@ export const products = [
         images: ['./img/img4.png', './img/img4-1.png', './img/img4-2.png', './img/img4-3.png'],
     },
     {
-        id: 'product-5',
+        id: 'product-11',
         name: '',
         price: 119.98,
-        discount: 79.93,
+        discount: 110.89,
         image: './img/img5.png',
         description: [
             {
@@ -124,10 +280,10 @@ export const products = [
         images: ['./img/img5.png', './img/img5-1.png', './img/img5-2.png', './img/img5-3.png', './img/img5-4.png'],
     },
     {
-        id: 'product-6',
+        id: 'product-12',
         name: 'Pasta Roller & Cutter',
         price: 148.00,
-        discount: 95.50,
+        discount: 120.88,
         image: './img/img6.png',
         description: [
             {
